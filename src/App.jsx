b7dd13203dc41309cd98b612fdbfe9d409f6551b
Anik Chowdhury Raj <acdevilliersanik17@gmail.com>
2024-08-24 11:31:02 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Card from './Components/card';
+import Header from './Components/header';
+import Footer from './Components/footer';
+
+function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+<Header />
+    
+      <Footer />
+
+    </>
+
+   
+  );
+}
+
+
+export default App;
